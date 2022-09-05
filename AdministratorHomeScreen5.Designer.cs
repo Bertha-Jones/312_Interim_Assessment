a9728b45fda8b480029b7f2aa5d913c350a770f9
Bertha-Jones <baajones@st.ug.edu.gh>
2022-09-05 11:45:23 +0000

    Made some Changes to admins home screen
    More functionality is added

---
+++ AdministratorHomeScreen5.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace InventorySystem2
+{
+    partial class AdministratorHomeScreen5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdministratorHomeScreen5));
+            this.label1 = new System.Windows.Forms.Label();
+            this.stockManagementButton = new System.Windows.Forms.Button();
+            this.categoryManagementButton = new System.Windows.Forms.Button();
+            this.adminNameTxt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.salesButton = new System.Windows.Forms.Button();
+            this.ProductManagemetButton = new System.Windows.Forms.Button();
+            this.userManagementButton = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(987, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // stockManagementButton
+            // 
+            this.stockManagementButton.BackColor = System.Drawing.Color.Black;
+            this.stockManagementButton.Font = new System.Drawing.Font("Elephant", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.stockManagementButton.ForeColor = System.Drawing.Color.Gold;
+            this.stockManagementButton.Location = new System.Drawing.Point(464, 465);
+            this.stockManagementButton.Name = "stockManagementButton";
+            this.stockManagementButton.Size = new System.Drawing.Size(331, 80);
+            this.stockManagementButton.TabIndex = 11;
+            this.stockManagementButton.Text = "Stock Management";
+            this.stockManagementButton.UseVisualStyleBackColor = false;
+            this.stockManagementButton.Click += new System.EventHandler(this.stockManagementButton_Click);
+            // 
+            // categoryManagementButton
+            // 
+            this.categoryManagementButton.BackColor = System.Drawing.Color.Black;
+            this.categoryManagementButton.Font = new System.Drawing.Font("Elephant", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.categoryManagementButton.ForeColor = System.Drawing.Color.Gold;
+            this.categoryManagementButton.Location = new System.Drawing.Point(464, 351);
+            this.categoryManagementButton.Name = "categoryManagementButton";
+            this.categoryManagementButton.Size = new System.Drawing.Size(331, 80);
+            this.categoryManagementButton.TabIndex = 10;
+            this.categoryManagementButton.Text = "Category Management";
+            this.categoryManagementButton.UseVisualStyleBackColor = false;
+            this.categoryManagementButton.Click += new System.EventHandler(this.categoryManagementButton_Click);
+            // 
+            // adminNameTxt
+            // 
+            this.adminNameTxt.AutoSize = true;
+            this.adminNameTxt.BackColor = System.Drawing.Color.Transparent;
+            this.adminNameTxt.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.adminNameTxt.ForeColor = System.Drawing.Color.Gold;
+            this.adminNameTxt.Location = new System.Drawing.Point(156, 22);
+            this.adminNameTxt.Name = "adminNameTxt";
+            this.adminNameTxt.Size = new System.Drawing.Size(101, 24);
+            this.adminNameTxt.TabIndex = 14;
+            this.adminNameTxt.Text = "adminName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1226, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 23);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(44, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Welcome Back";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.adminNameTxt);
+            this.panel2.Location = new System.Drawing.Point(0, -4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1262, 57);
+            this.panel2.TabIndex = 17;
+            // 
+            // salesButton
+            // 
+            this.salesButton.BackColor = System.Drawing.Color.Black;
+            this.salesButton.Font = new System.Drawing.Font("Elephant", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.salesButton.ForeColor = System.Drawing.Color.Gold;
+            this.salesButton.Location = new System.Drawing.Point(464, 606);
+            this.salesButton.Name = "salesButton";
+            this.salesButton.Size = new System.Drawing.Size(331, 80);
+            this.salesButton.TabIndex = 12;
+            this.salesButton.Text = "Sales Management";
+            this.salesButton.UseVisualStyleBackColor = false;
+            this.salesButton.Click += new System.EventHandler(this.salesButton_Click);
+            // 
+            // ProductManagemetButton
+            // 
+            this.ProductManagemetButton.BackColor = System.Drawing.Color.Black;
+            this.ProductManagemetButton.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProductManagemetButton.ForeColor = System.Drawing.Color.Gold;
+            this.ProductManagemetButton.Location = new System.Drawing.Point(464, 237);
+            this.ProductManagemetButton.Name = "ProductManagemetButton";
+            this.ProductManagemetButton.Size = new System.Drawing.Size(331, 80);
+            this.ProductManagemetButton.TabIndex = 9;
+            this.ProductManagemetButton.Text = "Product Management";
+            this.ProductManagemetButton.UseVisualStyleBackColor = false;
+            this.ProductManagemetButton.Click += new System.EventHandler(this.ProductManagemetButton_Click);
+            // 
+            // userManagementButton
+            // 
+            this.userManagementButton.BackColor = System.Drawing.Color.Black;
+            this.userManagementButton.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.userManagementButton.ForeColor = System.Drawing.Color.Gold;
+            this.userManagementButton.Location = new System.Drawing.Point(464, 114);
+            this.userManagementButton.Name = "userManagementButton";
+            this.userManagementButton.Size = new System.Drawing.Size(331, 80);
+            this.userManagementButton.TabIndex = 8;
+            this.userManagementButton.Text = "User Management";
+            this.userManagementButton.UseVisualStyleBackColor = false;
+            this.userManagementButton.Click += new System.EventHandler(this.userManagementButton_Click);
+            // 
+            // AdministratorHomeScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 726);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.salesButton);
+            this.Controls.Add(this.stockManagementButton);
+            this.Controls.Add(this.categoryManagementButton);
+            this.Controls.Add(this.ProductManagemetButton);
+            this.Controls.Add(this.userManagementButton);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1280, 726);
+            this.Name = "AdministratorHomeScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ss";
+            this.Load += new System.EventHandler(this.AdministratorHomeScreen_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button stockManagementButton;
+        private Button categoryManagementButton;
+        private Label adminNameTxt;
+        private Label label2;
+        private Label label3;
+        private Panel panel2;
+        private Button salesButton;
+        private Button ProductManagemetButton;
+        private Button userManagementButton;
+    }
+}
