30bf1f014d7a177b5c71f4f4038abc736dd25560
Bertha-Jones <baajones@st.ug.edu.gh>
2022-09-05 11:40:37 +0000

    Create AdministrationHomeScreen1.cs
    Added more functionalities

---
+++ AdministrationHomeScreen1.cs
@@ -0,0 +1,10 @@
+﻿namespace InventorySystem2
+{
+    internal class AdministrationHomeScreen1
+    {
+        internal void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
